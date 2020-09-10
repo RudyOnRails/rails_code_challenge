89d02234f451961fb73f9e6894756685ec1d6a65
Kevin Musiorski <RudyOnRails@gmail.com>
2020-09-10 14:33:59 -0500

    associate OrderItem fixture correctly to polymorphic type and add test on Product::sales_by_date

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
+include ActionView::Helpers::NumberHelper
+
 class ProductTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "::sales_by_date" do
+    assert_equal "$19.98", number_to_currency(Product.sales_by_date(10.years.ago..1.years.from_now).
+      first.order_items.sum{ |oi| oi.quantity * oi.price })
+    assert_equal 1, Product.sales_by_date(10.years.ago..1.years.from_now).length
   end
 end
